0f04bcb59f47bc2d15f5af44b7ad9c1e3d1d7e9c
bmathias12 <brandonmathias1@gmail.com>
2017-03-25 21:16:17 -0400

    added prediction density plot

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import os
-
-import numpy as np
-import pandas as pd
-from sklearn import metrics
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 from modutils import plotting
-
 
 RANDOM_STATE = 123
 
@@ -26,16 +20,12 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=RANDOM_STATE
     )
 
-
 clf = LogisticRegression(random_state=RANDOM_STATE)
 clf.fit(X_train, y_train)
 
 preds = clf.predict(X_test)
 probs = clf.predict_proba(X_test)[:,1]
 
-probs
-
-from importlib import reload
-reload(plotting)
-
 plotting.plot_roc_curve(y_test, probs)
+
+plotting.plot_prediction_density(y_test, probs)
--- modutils/plotting.py
+++ modutils/plotting.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import gaussian_kde
 from sklearn import metrics
 
 
@@ -15,6 +16,7 @@ def plot_roc_curve(
     """Function to plot a ROC curve when given true values and scores"""
     fpr, tpr, _ = metrics.roc_curve(y, scores, pos_label=pos_label)
     auc = metrics.roc_auc_score(y, scores)
+    
     fig, ax = plt.subplots(figsize=figsize)
     ax.plot(fpr, tpr, label='Area Under Curve: {:0.3f}'.format(auc))
     ax.plot([0,1], [0,1], color='black', linestyle='--')
@@ -22,7 +24,38 @@ def plot_roc_curve(
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.legend(loc='lower right')
+    
     if saveloc:
         plt.savefig(saveloc, bbox_inches='tight')
     else:
         plt.show()
+
+def plot_prediction_density(
+    y,
+    scores,
+    title='Prediction Density',
+    figsize=(8,5),
+    saveloc=None):
+    """Function to plot the kernel density for model predictions"""
+    
+    class_set = sorted(set(y))
+    if len(class_set) > 2:
+        raise ValueError('Function defined for binary classification only')
+    
+    x_grid = np.linspace(0, 1, 1000)
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    for v in class_set:
+        a = scores[y == v]
+        kernel = gaussian_kde(a, bw_method='scott')
+        kde = kernel.evaluate(x_grid)
+        ax.plot(x_grid, kde, linewidth=2.5, label='Target = {}'.format(v))
+        ax.fill_between(x_grid, kde, alpha=0.6)
+    plt.title(title, fontweight='semibold')
+    plt.legend()
+    
+    if saveloc:
+        plt.savefig(saveloc, bbox_inches='tight')
+    else:
+        plt.show()
+    
