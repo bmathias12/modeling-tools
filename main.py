ac2afd82512197792a9c4146c87ef3c7bbf3d0ab
bmathias12 <brandonmathias1@gmail.com>
2017-03-26 12:31:57 -0400

    Added quantile functions

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+
+import pandas as pd
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 from modutils import plotting
+from modutils import quantiles
 
 RANDOM_STATE = 123
 
@@ -26,6 +29,15 @@ clf.fit(X_train, y_train)
 preds = clf.predict(X_test)
 probs = clf.predict_proba(X_test)[:,1]
 
+# Check Plotting Functions
 plotting.plot_roc_curve(y_test, probs)
-
 plotting.plot_prediction_density(y_test, probs)
+
+# Check quantile functions
+bins = quantiles.get_bins(probs, q=10, adjust_endpoints=True)
+deciles = quantiles.get_quantiles_from_bins(probs, bins)
+
+# Check that mean by decile is descending from 1 to 10
+d = pd.concat([pd.Series(probs), deciles], axis=1)
+d.columns = ['scores', 'deciles']
+d.groupby('deciles')['scores'].mean()
---
+++ modutils/quantiles.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+def get_bins(scores, q=10, adjust_endpoints=False):
+    _, bins =  pd.qcut(scores, q=q, retbins=True)
+    if adjust_endpoints:
+        bins[0] = 0.0
+        bins[len(bins)-1] = 1.0
+    return bins
+
+def get_quantiles_from_bins(scores, bins, one_high=True):
+    quantiles = pd.Series(np.digitize(scores, bins, right=True))
+    if one_high:
+        q = len(bins)-1
+        quantiles = q - quantiles + 1
+    return quantiles
+    
+    
